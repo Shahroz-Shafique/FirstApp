487239f9cca821f452af58c32d6db98c19252b18
Shahroz-Shafique <201370121@gift.edu.pk>
2023-08-25 22:48:17 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-    <h1>SHAHROZ M SHAFIQUE</h1>
-    <h1>201370121</h1>
+      <h2>SHAHROZ M SHAFIQUE</h2>
+      <h2>201370121</h2>
     </View>
   );
 }
@@ -15,8 +15,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
